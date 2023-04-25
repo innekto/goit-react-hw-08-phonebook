25346e6f9c63473a218b80f191fd74baa096b8fd
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-04-25 20:14:21 +0300

    d

--- src/components/App/App.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  width: 600px;
-  margin-right: auto;
-  margin-left: auto;
-  padding: 10px 40px 10px 40px;
-  text-align: center;
-`;
--- src/redux/theme/themeSelector.js
+++
@@ -1,1 +0,0 @@
-export const selectApplicationTheme = state => state.theme.darkTheme;
